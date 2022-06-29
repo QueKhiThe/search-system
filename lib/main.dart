8d1b61e1d3162a5779f5b5349efe9bfb82d63c79
QueDinhT <dinhtrongque01071996@gmail.com>
2022-06-29 23:16:34 +0700

    handle data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:search_system/models/movie_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,6 +24,81 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  static List<Movie> moviesList = [
+    Movie(
+        "Người Sắt",
+        2008,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.9),
+    Movie(
+        "Người nhện",
+        2018,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.3),
+    Movie(
+        "Thor",
+        2022,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.4),
+    Movie(
+        "Phù thuỷ áo đen",
+        2012,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        7.6),
+    Movie(
+        "Biệt đội siêu anh hùng",
+        2011,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        5.6),
+    Movie(
+        "Nông dân",
+        2012,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        7.3),
+    Movie(
+        "Chiến binh mùa đông",
+        2008,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.9),
+    Movie(
+        "Siêu nhân gao",
+        2008,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.9),
+    Movie(
+        "Cảnh sát vùng biển",
+        2018,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.9),
+    Movie(
+        "Kho báu khổng lồ",
+        2019,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.9),
+    Movie(
+        "Trở về quá khứ",
+        2020,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.9),
+    Movie(
+        "Tập làm người lớn",
+        2022,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.9),
+    Movie(
+        "Chiến binh mùa đông",
+        2008,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.9),
+    Movie(
+        "Siêu nhân gao",
+        2008,
+        "https://s3.cloud.cmctelecom.vn/tinhte2/2019/10/4799623_Cover_Ironman.jpg",
+        8.9),
+  ];
+
+  List<Movie> movies = List.from(moviesList);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,7 +132,26 @@ class _SearchPageState extends State<SearchPage> {
                       borderRadius: BorderRadius.circular(8),
                       borderSide: BorderSide.none),
                   hintText: "ex: The spider man"),
-            )
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: movies.length,
+                    itemBuilder: (context, index) => ListTile(
+                          contentPadding: const EdgeInsets.all(8),
+                          title: Text(
+                            movies[index].title!,
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                          subtitle: Text("${movies[index].releaseYear}"),
+                          trailing: Text("${movies[index].rating}"),
+                          leading: Image.network(movies[index].url!),
+                        )))
           ],
         ),
       ),
